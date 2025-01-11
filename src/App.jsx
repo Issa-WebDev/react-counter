179c708404400acd0f7d1c19a26816f4275c242d
Issa-WebDev <issawebdev@gmail.com>
2025-01-11 19:05:02 +0100

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // Counter
 import React, { useState } from "react";
+import "./style.css";
 
 const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>COUNTER</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <h1 className="title">COUNTER</h1>
+      <h1 className="count">{count}</h1>
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button className="btn" onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
