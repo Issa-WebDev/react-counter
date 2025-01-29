4a9461bbb4ddec592c6c37d582270b18a2d169cb
Issa-WebDev <kissadev10@gmail.com>
2025-01-29 12:19:25 +0100

    add count in the local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 // Counter
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => {
+    return Number(localStorage.getItem("count")) || 0;
+  });
+
+  const decrement = () => setCount((c) => c - 1);
+  const increment = () => setCount((c) => c + 1);
+
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]);
+
   return (
     <div>
       <h1 className="title">COUNTER</h1>
       <h1 className="count">{count}</h1>
-      <button className="btn" onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-      <button className="btn" onClick={() => setCount(count - 1)}>
-        Decrement
-      </button>
+
+      <button onClick={decrement}>➖</button>
+      <button onClick={increment}>➕</button>
     </div>
   );
 };
